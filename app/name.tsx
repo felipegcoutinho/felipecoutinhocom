9076f8a010d54d9f33c71448d6348e78e7214657
Felipe <felipi@live.com>
2025-03-30 20:38:19 -0300

    Refactor layout and update dependencies

--- app/name.tsx
+++ app/name.tsx
@@ -1,4 +1,4 @@
-import { unstable_ViewTransition as ViewTransition } from 'react';
+import { unstable_ViewTransition as ViewTransition } from "react";
 
 export function NameTransition() {
   return (
@@ -9,19 +9,19 @@ export function NameTransition() {
           aria-hidden="true"
           className="block overflow-hidden group relative"
         >
-          <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
-            {'Lee Robinson'.split('').map((letter, index) => (
+          <span className="text-3xl inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
+            {"Felipe Coutinho".split("").map((letter, index) => (
               <span
                 key={index}
                 className="inline-block"
                 style={{ transitionDelay: `${index * 25}ms` }}
               >
-                {letter === ' ' ? '\u00A0' : letter}
+                {letter === " " ? "\u00A0" : letter}
               </span>
             ))}
           </span>
-          <span className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
-            {'leerob'.split('').map((letter, index) => (
+          <span className="text-3xl inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
+            {"felipegcoutinho".split("").map((letter, index) => (
               <span
                 key={index}
                 className="inline-block"
